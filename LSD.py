bc094e351e5c5598bbb5b03892c62b84b870e712
MikeCollasa <79419352+MikeCollasa@users.noreply.github.com>
2021-03-02 14:37:54 +0100

    Update LSD.py

--- LSD.py
+++ LSD.py
@@ -67,9 +67,10 @@ done""")
 os.system("mkdir derep && mv *derep* derep/")
 
 ###Denoising:
+### You can use -minsize option for example if you have very low number of reads in your libraries
 os.system("""for file in *sorted.fasta; do
     SampleName=`basename $file .sorted.fasta`
-    usearch -unoise3 $SampleName.sorted.fasta -zotus $SampleName.zotus.fasta -tabbedout $SampleName.denoising.summary.txt -minsize 1
+    usearch -unoise3 $SampleName.sorted.fasta -zotus $SampleName.zotus.fasta -tabbedout $SampleName.denoising.summary.txt
 done""")
 
 os.system("mkdir sorted && mv *sorted.fasta sorted/")
